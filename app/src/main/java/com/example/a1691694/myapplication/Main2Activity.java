ab04a1d35d12133bfce62129ca5052cb992e7e91
Jaspreet Singh <1691694@CEGEPGIM.LCL>
2017-05-18 16:05:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1691694/myapplication/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.a1691694.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void Someclick(View v)
+
+    {
+
+        Intent intent = new Intent(this,Main3Activity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/example/a1691694/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a1691694.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Someclick(View v)
+
+    {
+
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
